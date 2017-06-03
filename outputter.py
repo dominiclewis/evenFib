685a939c9db403b0bf12279be02108be551134d6
Dominic Lewis <dominic@Dominics-MacBook-Air.local>
2017-06-03 18:50:33 +0100

    updated evenTotal to init =2 as prob def was unclear

--- outputter.py
+++ outputter.py
@@ -2,7 +2,7 @@ def main():
     num1 = 1
     num2 = 2
     temp = 0
-    evenTotal = 0
+    evenTotal = 2
 
     recurseFib(num1,num2,temp,evenTotal)
 
@@ -10,17 +10,17 @@ def main():
 
 
 def recurseFib(num1,num2,temp,evenTotal):
-    print("{}".format(num1))
+
     temp = num1 + num2
-    if ((temp <= 4000000) and (temp % 2 == 0)): #if the curTotal is even
+    if ((num2 <= 4000000) and (temp % 2 == 0)): #if the curTotal is even
         evenTotal += temp
-    num1 = num2
-    num2 = temp
 
     if (num2 <= 4000000):
+        num1 = num2
+        num2 = temp
         recurseFib(num1,num2,temp,evenTotal)
 
-
+    print("{}".format(evenTotal))
 
 
 
